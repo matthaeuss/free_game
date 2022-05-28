6048a31af2840af2db531886a79895dc63da960a
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-05-28 18:09:03 +0200

    custom hook useFetch added

---
+++ src/components/GameList/GameList.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+function GameList() {
+
+const [games, setGames] = useState([]);
+
+
+    return (
+        <div>GameList</div>
+    );
+}
+
+export default GameList;
