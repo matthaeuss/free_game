d44d21232777e957440f5eb5d47d0ded686f5822
matthaeuss <91155894+matthaeuss@users.noreply.github.com>
2022-05-29 08:54:40 +0200

    component filter completed

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -6,6 +6,7 @@ function Filters({setFilter}) {
     const [platform, setPlatform] = useState(PLATFORMS[0].value);
     const [genre, setGenre] = useState(GENRES[0].value);
     const [tags, setTags] = useState(TAGS[0].value);
+    const [sortBy, setSortBy] = useState(SORT_BY[0].value);
 
     function handlePlatform(e) {
         setPlatform(e.target.value);
@@ -19,6 +20,11 @@ function Filters({setFilter}) {
 
     function handleTags(e) {
         setTags(e.target.value);
+        setFilter((prevState) => ({...prevState, [e.target.name]: e.target.value}))
+    }
+
+    function handleSortBy(e) {
+        setSortBy(e.target.value);
         setFilter((prevState) => ({...prevState, [e.target.name]: e.target.value}))
     }
 
@@ -52,7 +58,7 @@ function Filters({setFilter}) {
             <select
                 name="tags"
                 id="tags"
-                value={platform}
+                value={tags}
                 onChange={handleTags}
             >
                 {TAGS.map(({value, display}) => (
@@ -60,12 +66,12 @@ function Filters({setFilter}) {
                 ))}
             </select>
 
-            <label htmlFor="sort_by">Sort: </label>
+            <label htmlFor="sort_by">Sort by: </label>
             <select
-                name="platform"
-                id="platform"
-                value={platform}
-                onChange={handlePlatform}
+                name="sort_by"
+                id="sort_by"
+                value={sortBy}
+                onChange={handleSortBy}
             >
                 {PLATFORMS.map(({value, display}) => (
                     <option value={value}>{display}</option>
